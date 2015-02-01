c8b4735478b8444ef8ae764124cd0630d4256ea1
dbrugne <damien.brugne@gmail.com>
2015-02-01 15:11:15 +0100

    Rotate the text on each timeout by setting their parent div top position

--- jquery.talkative.js
+++ jquery.talkative.js
@@ -18,6 +18,11 @@
 			if (alternatives.length < 1)
 			  return;
 
+			$(alternatives).css('display', 'block'); // doing this by JS avoid all content to be displayed on page load
+
+			var $container = $('<div></div>').appendTo($el);
+			$(alternatives).prependTo($container);
+
 			// adjust container height
 			var $first = $(alternatives[0]);
 			var h = $first.outerHeight(true); // full element height including margins
@@ -30,64 +35,14 @@
 					return console.log('Unable to find next alternative for '+$el);
 
 				var $alternative = $(alternative);
-				console.log($alternative.text());
+
+				var top = $alternative.position().top;
+				$container.css('top', '-'+top+'px');
+
+				console.log($alternative.text(),top);
 
 				var timeout = setTimeout(rotate, settings.delay);
 			}
-
-			/**
-						 function transformById(elmtId, otherVerb, fadeDuration){ //replaces the innerHTML of the pased element by the passed string
-				if(!fadeDuration){
-					fadeDuration = 0;
-				}
-				document.getElementById(elmtId).style.opacity = '0';
-				setTimeout(function(){
-					document.getElementById(elmtId).innerHTML = otherVerb;
-					document.getElementById(elmtId).style.opacity = '1';
-				}, fadeDuration);
-			}
-
-						 function startTalkative(){
-				var pathToXML = 'talkative.xml';
-				var lang = document.getElementsByTagName('html')[0].lang
-				lang==''? lang='en' : lang;
-				var req = new XMLHttpRequest();
-				req.open('GET', pathToXML, true);
-				req.send();
-
-				req.onreadystatechange=function(){
-					if(req.readyState == 4 && req.status == 200){
-
-						var xml = req.responseXML;
-						var xmlLang = xml.getElementsByTagName(lang)[0];
-						var configs = xml.getElementsByTagName('cs')[0];
-						var verbs = xmlLang.getElementsByTagName('v');
-						var interests = xmlLang.getElementsByTagName('i');
-						var targets = xmlLang.getElementsByTagName('t');
-						var verbTimer = configs.getElementsByTagName('verbTimer')[0].innerHTML;
-						var interestTimer = configs.getElementsByTagName('interestTimer')[0].innerHTML;
-						var targetTimer = configs.getElementsByTagName('targetTimer')[0].innerHTML;
-						var fadeDuration = configs.getElementsByTagName('fadeDuration')[0].innerHTML;
-						var startingDelay = configs.getElementsByTagName('startingDelay')[0].innerHTML;
-						document.getElementById('verb').style.transition = (fadeDuration/1000)+'s';
-						document.getElementById('interest').style.transition = (fadeDuration/1000)+'s';
-						document.getElementById('target').style.transition = (fadeDuration/1000)+'s';
-
-						setTimeout(function(){
-							var verbInterval = setInterval(function(){
-								transformById('verb', getRandomFromArray(verbs).innerHTML, fadeDuration/2);
-							}, verbTimer);
-							var interestInterval = setInterval(function(){
-								transformById('interest', getRandomFromArray(interests).innerHTML, fadeDuration/2);
-							}, interestTimer);
-							var targetInterval = setInterval(function(){
-								transformById('target', getRandomFromArray(targets).innerHTML, fadeDuration/2);
-							}, targetTimer);
-						}, startingDelay)
-					}
-				}
-			}
-			 */
 		});
 
 		function getRandomFromjQueryObject(arr) {
