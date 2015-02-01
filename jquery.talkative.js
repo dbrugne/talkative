37c88ef92587323b93c88ee40adee73b4bc124ba
dbrugne <damien.brugne@gmail.com>
2015-02-01 15:41:03 +0100

    Animate the rotation

--- jquery.talkative.js
+++ jquery.talkative.js
@@ -7,20 +7,23 @@
 			selector: 'span',
 			start: 1000,  // first rotation after (ms)
 			delay: 4000, // delay between rotation (ms)
-			animation: true
+			animation_delay: 1000
 		}, options );
+
+		if (settings.animation_delay < 100)
+			settings.animation_delay = 100;
+		if (settings.delay <= settings.animation_delay)
+			settings.delay = settings.animation_delay + 100;
 
 		this.filter( "div, p" ).each(function() {
 			var $el = $(this);
-			$el.css('color', 'red');
 
 			var alternatives = $el.find(settings.selector);
 			if (alternatives.length < 1)
 			  return;
 
-			$(alternatives).css('display', 'block'); // doing this by JS avoid all content to be displayed on page load
-
 			var $container = $('<div></div>').appendTo($el);
+			var color = $container.css('color');
 			$(alternatives).prependTo($container);
 
 			// adjust container height
@@ -34,12 +37,21 @@
 				if (!alternative)
 					return console.log('Unable to find next alternative for '+$el);
 
+				// calculate alternative position
 				var $alternative = $(alternative);
-
 				var top = $alternative.position().top;
-				$container.css('top', '-'+top+'px');
 
-				console.log($alternative.text(),top);
+				// blur
+				$container.addClass('roll');
+
+				// animate
+				$container.animate({
+					'top': '-'+top+'px'
+				}, settings.animation_delay, function() {
+					$container.removeClass('roll');
+				});
+
+				//console.log($alternative.text(),top);
 
 				var timeout = setTimeout(rotate, settings.delay);
 			}
